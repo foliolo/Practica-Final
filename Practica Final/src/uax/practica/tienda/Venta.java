8781673bd6ef30890bbcda6e74679da20c78b5f7
Portatil <Portatil@Portatil-PC>
2013-06-29 21:19:22 +0200

    Inicio de la clase Venta

--- Practica Final/src/uax/practica/tienda/Venta.java
+++ Practica Final/src/uax/practica/tienda/Venta.java
@@ -1,10 +1,15 @@
 package uax.practica.tienda;
 
 /**
- * Clase principal de entrada al programa
+ * Clase que se encarga del tratamiento de las ventas.
  * @version v0.1 	24 Junio 2013
  * @author Alberto Hidalgo
  */
 public class Venta {
+	
+	
+	Venta(){
+		
+	}
 
 }
